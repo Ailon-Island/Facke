754db29f189f9c98479bb67ed9b9e1c159838af4
Ailon-Island <74121851+Ailon-Island@users.noreply.github.com>
2022-04-18 14:26:51 +0800

    Update train_SimSwap.py

--- train_SimSwap.py
+++ train_SimSwap.py
@@ -142,6 +142,9 @@ def test(opt, model, loader, epoch_idx, total_iter):
         else:
             [losses, _] = model(img_source, img_target, latent_ID, latent_ID_target)
         losses = [loss.detach().cpu().item() for loss in losses]
+        
+        # gather losses
+        losses = [torch.mean(x) if not isinstance(x, int) else x for x in losses]
 
         # save loss
         if not len(test_losses):
