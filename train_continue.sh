fe86f80c96dc5f5051269b676da08ed11fb15d95
Ailon-Island <ailon_jw@sjtu.edu.cn>
2022-04-19 12:34:48 +0800

    shell code for running on server

---
+++ train_continue.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+#SBATCH -J Facke
+#SBATCH -p gpu
+#SBATCH -N 1
+#SBATCH -o log.out
+#SBATCH -t 10-10:00:00
+#SBATCH --gres=gpu:2
+module load anaconda3/2019.07
+source activate pytorch_1.11
+python -u ./train_SimSwap.py --batchSize 32 --num_workers 64 --continue_train
