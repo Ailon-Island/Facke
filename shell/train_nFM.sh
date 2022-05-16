15430ca56e87c8c4e901aeef169901b5b2e9e923
Ailon-Island <ailon_jw@sjtu.edu.cn>
2022-05-16 19:09:33 +0800

    hah

---
+++ shell/train_nFM.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+#SBATCH -J Facke_oFM
+#SBATCH -p gpu
+#SBATCH -N 1
+#SBATCH -o log.out
+#SBATCH -t 10-10:00:00
+#SBATCH --gres=gpu:2
+module load anaconda3/2019.07
+source activate pytorch_1.11
+python -u ./train_SimSwap.py --batchSize 32 --nThreads 32 --name SimSwap_oFM --no_ganFeat_loss
