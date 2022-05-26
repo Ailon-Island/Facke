06128dc58f016388cfb9e32a4414de7dc649955e
Ailon-Island <ailon_jw@sjtu.edu.cn>
2022-05-26 10:44:22 +0800

    shell

--- shell/train_finetune_DDPM.sh
+++ shell/train_finetune_DDPM.sh
@@ -7,4 +7,4 @@
 #SBATCH --gres=gpu:2
 module load anaconda3/2019.07
 source activate pytorch_1.11
-python -u finetune_DDPM.py --attention_resolutions 16 --diffusion_steps 1000 --dropout 0.0 --image_size 256 --learn_sigma --noise_schedule linear --num_channels 128 --num_head_channels 64 --num_res_blocks 1 --resblock_updown --use_scale_shift_norm --timestep_respacing 100 --DDPM_pth .\utils\guided_diffusion\models\ffhq_10m.pt --down_N 16 --range_t 20 --clip_denoised --batchSize 8 --name Facke_finetune_DDPM
+python -u finetune_DDPM.py --attention_resolutions 16 --diffusion_steps 1000 --dropout 0.0 --image_size 256 --learn_sigma --noise_schedule linear --num_channels 128 --num_head_channels 64 --num_res_blocks 1 --resblock_updown --use_scale_shift_norm --timestep_respacing 100 --DDPM_pth .//utils//guided_diffusion//models//ffhq_10m.pt --down_N 16 --range_t 20 --clip_denoised --batchSize 8 --name Facke_finetune_DDPM --lr 2e-4
