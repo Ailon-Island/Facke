7f68f89fc49818fc016bdff627ba016988cb820a
Dong <a_Dong@sjtu.edu.cn>
2022-05-28 09:00:37 +0800

    more shell

---
+++ shell/train_CVAE.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+#SBATCH -J Facke_CVAE_New_Generator
+#SBATCH -p gpu
+#SBATCH -N 1
+#SBATCH -o log.out
+#SBATCH -t 10-10:00:00
+#SBATCH --gres=gpu:2
+module load anaconda3/2019.07
+source activate pytorch_1.11
+python -u ./train_CVAE.py --model CVAE --batchSize 32 --name CVAE_New_Generateor_ID --display_freq 10000 --no_intra_ID_random --lambda_id 1 --print_freq 4000
+
+
