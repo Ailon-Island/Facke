5bfb95b142baa5c51ab7c699fd562b0c800c6ea2
Dong <a_Dong@sjtu.edu.cn>
2022-06-18 23:11:07 +0800

    skip DDPM benchmark

--- shell/benchmark_ILVR.sh
+++ shell/benchmark_ILVR.sh
@@ -7,4 +7,4 @@
 #SBATCH --gres=gpu:1
 module load anaconda3/2019.07
 source activate pytorch_1.11
-python -u benchmark.py --attention_resolutions 16 --diffusion_steps 1000 --dropout 0.0 --image_size 256 --learn_sigma --noise_schedule linear --num_channels 128 --num_head_channels 64 --num_res_blocks 1 --resblock_updown --use_scale_shift_norm --timestep_respacing 100 --DDPM_pth ./utils/guided_diffusion/models/ffhq_10m.pt --down_N 32 --range_t 20 --clip_denoised --batchSize 32 --name ILVR_pretrained --epoch_label pretrained --model ILVR
+python -u benchmark.py --benchmark_skip 4 --benchmark_coarse 64 --benchmark_fine 320 --attention_resolutions 16 --diffusion_steps 1000 --dropout 0.0 --image_size 256 --learn_sigma --noise_schedule linear --num_channels 128 --num_head_channels 64 --num_res_blocks 1 --resblock_updown --use_scale_shift_norm --timestep_respacing 100 --DDPM_pth ./utils/guided_diffusion/models/ffhq_10m.pt --down_N 32 --range_t 20 --clip_denoised --batchSize 32 --name ILVR_pretrained --epoch_label pretrained --model ILVR
