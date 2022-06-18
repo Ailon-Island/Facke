885a751f6b55ffb86a8513c049af8d4a2206d6d6
Dong <a_Dong@sjtu.edu.cn>
2022-06-18 15:14:25 +0800

    modify shell for DDPM

--- shell/benchmark_finetuned_ILVR_lr5e-5.sh
+++ shell/benchmark_finetuned_ILVR_lr5e-5.sh
@@ -7,4 +7,4 @@
 #SBATCH --gres=gpu:1
 module load anaconda3/2019.07
 source activate pytorch_1.11
-python -u demo_ILVR.py --attention_resolutions 16 --diffusion_steps 1000 --dropout 0.0 --image_size 256 --learn_sigma --noise_schedule linear --num_channels 128 --num_head_channels 64 --num_res_blocks 1 --resblock_updown --use_scale_shift_norm --timestep_respacing 100 --DDPM_pth ./checkpoints/Facke_finetune_DDPM/DDPM/latest.pth --down_N 32 --range_t 20 --clip_denoised --batchSize 8 --name Facke_finetune_DDPM_lr5e-5
+python -u benchmark.py --attention_resolutions 16 --diffusion_steps 1000 --dropout 0.0 --image_size 256 --learn_sigma --noise_schedule linear --num_channels 128 --num_head_channels 64 --num_res_blocks 1 --resblock_updown --use_scale_shift_norm --timestep_respacing 100 --DDPM_pth ./utils/guided_diffusion/models/ffhq_10m.pt --down_N 32 --range_t 20 --clip_denoised --batchSize 32 --name Facke_finetune_DDPM_lr5e-5 --model ILVR
